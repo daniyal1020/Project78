6345c6613581c267a863b3ee06dfcd9e943b115d
daniyal1020 <107762034+daniyal1020@users.noreply.github.com>
2022-06-23 21:32:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
 var i = 0;
 function update()
 {
@@ -11,9 +11,9 @@ function update()
       }
     
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [images];
+    var updatedImage = images[i];
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [names] ;
+    var updatedName = names[i] ;
  
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
